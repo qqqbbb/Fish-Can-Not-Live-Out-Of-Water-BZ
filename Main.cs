0eabceed00a1ad466dc6aa0b16c030d29668f626
qqqbbb <qqqinbox@ya.ru>
2022-11-24 09:48:25 +0300

    2.0

--- Main.cs
+++ Main.cs
@@ -14,8 +14,8 @@ namespace Fish_Out_Of_Water
     public class Main
     {
         internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
-        static bool gameLoaded = false;
-        public static PDA pda;
+        //static bool gameLoaded = false;
+        //public static PDA pda;
 
         public static void Log(string str, QModManager.Utility.Logger.Level lvl = QModManager.Utility.Logger.Level.Debug)
         {
@@ -24,55 +24,8 @@ namespace Fish_Out_Of_Water
 
         public static void Setup()
         {
-            Player.main.isUnderwaterForSwimming.changedEvent.AddHandler(Player.main, new UWE.Event<Utils.MonitoredValue<bool>>.HandleFunction(Fish_Out_Of_Water.OnPlayerIsUnderwaterForSwimmingChanged));
-            pda = Player.main.GetPDA();
-            Fish_Out_Of_Water.AddFishToList();
-        }
-
-        //[HarmonyPatch(typeof(Player), "TrackTravelStats")]
-        internal class Player_TrackTravelStats_Patch
-        { // 
-            public static void Postfix(Player __instance)
-            {
-                if (!gameLoaded)
-                {
-                    AddDebug(" Loaded !!!!!!!!!!!!!");
-                    Setup();
-                    gameLoaded = true;
-                }
-            }
-        }
-        //[HarmonyPatch(typeof(Player), "Start")]
-        internal class Player_Start_Patch
-        { // 
-            public static void Postfix(Player __instance)
-            {
-                if (!gameLoaded)
-                {
-                    AddDebug(" Loaded !!!!!!!!!!!!!");
-                    Setup();
-                    gameLoaded = true;
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(uGUI_SceneLoading), "End")]
-        internal class uGUI_SceneLoading_End_Patch
-        { // when loading savegame runs more than once
-            public static void Postfix(uGUI_SceneLoading __instance)
-            {
-                //if (!uGUI.main.hud.active)
-                //{
-                //    AddDebug(" Loading");
-                //    return;
-                //}
-                //if (!gameLoaded)
-                //{
-                    //AddDebug(" Loaded !!!!!!!!!!!!!");
-                    Setup();
-                    //gameLoaded = true;
-                //}
-            }
+            //Player.main.isUnderwaterForSwimming.changedEvent.AddHandler(Player.main, new UWE.Event<Utils.MonitoredValue<bool>>.HandleFunction(Fish_Out_Of_Water.OnPlayerIsUnderwaterForSwimmingChanged));
+            //pda = Player.main.GetPDA();
         }
 
         [HarmonyPatch(typeof(IngameMenu), "QuitGameAsync")]
@@ -81,7 +34,25 @@ namespace Fish_Out_Of_Water
             public static void Postfix(IngameMenu __instance, bool quitToDesktop)
             {
                 if (!quitToDesktop)
+                {
+                    //AddDebug(" QuitGameAsync");
                     Fish_Out_Of_Water.fishOutOfWater = new Dictionary<LiveMixin, float>();
+                    Fish_Out_Of_Water.fishInInventory = new Dictionary<LiveMixin, float>();
+                }
+            }
+        }
+
+        //[HarmonyPatch(typeof(WaitScreen), "Hide")]
+        internal class WaitScreen_Hide_Patch
+        { // fires after game loads
+            public static void Postfix(WaitScreen __instance)
+            {
+                //AddDebug(" WaitScreen Hide");
+                //if (uGUI.isLoading)
+                {
+                    //AddDebug(" WaitScreen Hide  !!!");
+                    Setup();
+                }
             }
         }
 
